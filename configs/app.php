f51081a052d6f61f8b9443fc1b1eac6a6539e315
Hesam <bahmankhah1@gmail.com>
2025-12-27 03:03:48 +0330

    fixed menus

---
+++ configs/app.php
@@ -0,0 +1,19 @@
+<?php
+
+use Dorostkar\Providers\AppServiceProvider;
+
+
+return [
+    'global_middlewares'=>[
+
+    ],
+    'providers'=>[
+        AppServiceProvider::class,
+    ],
+    'version'=>'v1',
+    'name'=>'donapp-core',
+    'url'=>getenv('APP_URL'),
+    'api'=> [
+        'namespace'=>'forooshyar/v1',
+    ],
+];
